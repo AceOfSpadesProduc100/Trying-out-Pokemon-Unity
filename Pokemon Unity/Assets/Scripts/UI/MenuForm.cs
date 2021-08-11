6f0b9f75e627de6d4f96f00e2d76641cf10077e1
AceOfSpadesProduc100 <37678351+AceOfSpadesProduc100@users.noreply.github.com>
2021-08-11 13:50:02 -0400

    Initialize UnityUI

---
+++ Pokemon Unity/Assets/Scripts/UI/MenuForm.cs
@@ -0,0 +1,67 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2019 Jiang Yin. All rights reserved.
+// Homepage: http://gameframework.cn/
+// Feedback: mailto:jiangyin@gameframework.cn
+//------------------------------------------------------------
+
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace PokemonUnity
+{
+    public class MenuForm : UGuiForm
+    {
+        [SerializeField]
+        private GameObject m_QuitButton = null;
+
+        private ProcedureMenu m_ProcedureMenu = null;
+
+        public void OnStartButtonClick()
+        {
+            m_ProcedureMenu.StartGame();
+        }
+
+        public void OnSettingButtonClick()
+        {
+            GameEntry.UI.OpenUIForm(UIFormId.SettingForm);
+        }
+
+        public void OnAboutButtonClick()
+        {
+            GameEntry.UI.OpenUIForm(UIFormId.AboutForm);
+        }
+
+        public void OnQuitButtonClick()
+        {
+            GameEntry.UI.OpenDialog(new DialogParams()
+            {
+                Mode = 2,
+                Title = GameEntry.Localization.GetString("AskQuitGame.Title"),
+                Message = GameEntry.Localization.GetString("AskQuitGame.Message"),
+                OnClickConfirm = delegate (object userData) { UnityGameFramework.Runtime.GameEntry.Shutdown(ShutdownType.Quit); },
+            });
+        }
+
+        protected override void OnOpen(object userData)
+        {
+            base.OnOpen(userData);
+
+            m_ProcedureMenu = (ProcedureMenu)userData;
+            if (m_ProcedureMenu == null)
+            {
+                Log.Warning("ProcedureMenu is invalid when open MenuForm.");
+                return;
+            }
+
+            m_QuitButton.SetActive(UnityEngine.Application.platform != RuntimePlatform.IPhonePlayer);
+        }
+
+        protected override void OnClose(bool isShutdown, object userData)
+        {
+            m_ProcedureMenu = null;
+
+            base.OnClose(isShutdown, userData);
+        }
+    }
+}
---
+++ PokemonUnity.Shared/Extensions/PokemonExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace PokemonUnity
+{
+	public static class PokemonExtension
+	{
+		public static bool IsNotNullOrNone(this PokemonUnity.Monster.Pokemon pokemon)
+		{
+			return pokemon != null && pokemon.Species != Pokemons.NONE; 
+		}
+		public static bool IsNotNullOrNone(this PokemonUnity.Battle.Pokemon pokemon)
+		{
+			return pokemon != null && pokemon.Species != Pokemons.NONE; 
+		}
+		public static string ToString(this PokemonUnity.Monster.Stats stat, TextScripts text)
+		{
+			//create a switch, and return Locale Name or Description
+			return stat.ToString();
+		}
+		public static string ToString(this PokemonUnity.Battle.Stats stat, TextScripts text)
+		{
+			//create a switch, and return Locale Name or Description
+			return stat.ToString();
+		}
+		public static string ToString(this PokemonUnity.Monster.Forms form, TextScripts text)
+		{
+			//create an operator and return Locale Name
+			return form.ToString();
+		}
+		public static string ToString(this PokemonUnity.Pokemons pokemon, TextScripts text)
+		{
+			//create a switch, and return Locale Name or Description
+			return pokemon.ToString();
+		}
+	}
+}
